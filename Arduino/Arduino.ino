d5086b33ba6fef006a5d4f52272cfd46e492e791
unknown <user@USER.Laptop>
2020-01-13 11:23:18 +0100

    Polisched things up [Untested]

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,16 +1,31 @@
 /*
   Program written by JelleWho
   Board: https://dl.espressif.com/dl/package_esp32_index.json
+
+  Dependencies (NOT INCLUDED!!, INCLUDE YOUR OWN OR INCLUDE IT YOURSELF)
+  https://github.com/jellewie/Arduino-WiFiManager
+  https://github.com/jellewie/Arduino_DIYHUE_switch
 */
+#define IFTTT
+
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <WebServer.h>
+
+WebServer server(80);
+WiFiClientSecure client;
 
 void setup() {
   WiFiManager_Start();      //Setup all WIFI stuff (NOT INCLUDED!!, INCLUDE YOUR OWN OR INCLUDE IT YOURSELF)
-  IFTTT_setup();            //start IFTTT, this will set the webser stuff up.
+  
+  server.on("/set",       IFTTT_handle_Set);
+  server.on("/register",  IFTTT_handle_Register);
+  server.begin();
+  
   //Serial.begin(115200);
   //Serial.println(WiFi.localIP()); //Just send it's IP on boot to let you know
 }
 
 void loop() {
-  IFTTT_loop();             //handle the IFTTT web trafic
+  server.handleClient();      //handle the (IFTTT) web trafic
 }
--- Arduino/IFTTT.ino
+++ Arduino/IFTTT.ino
@@ -1,48 +1,23 @@
 /* Written by JelleWho https://github.com/jellewie
-
-   TODO
-   Polish it up, test it even more
-   Get data back from Raspberry?
-
+   
    DO NOT USE:
    Prefix 'IFTTT' for anything
 */
-
-#include <WiFiClientSecure.h>
-#include <WebServer.h>
-//#include <IFTTTMaker.h>
-
-//#define IFTTT_SerialEnabled
-
-//#define IFTTT_KEYIFTTT "REPLACE THIS WITH YOU IFTTT KEY"
-//IFTTTMaker ifttt(IFTTT_KEYIFTTT, client);
-
-WebServer IFTTT_server(80);
-WiFiClientSecure client;
-
-void IFTTT_setup() {
-  IFTTT_server.on("/set",       IFTTT_handle_Set);
-  IFTTT_server.on("/register",  IFTTT_handle_Register);
-  IFTTT_server.onNotFound(      IFTTT_HandleNotFound);
-  IFTTT_server.begin();
-}
-void IFTTT_loop() {
-  IFTTT_server.handleClient();
-}
+#ifdef IFTTT
 void IFTTT_handle_Register() {
   //URL argument names like "<IP>/register?mac=<value>"
   //Example                 "192.168.1.128/register?mac=00:0a:95:9d:68:16"
 
-  for (int i = 0; i < IFTTT_server.args(); i++) {
-    String IFTTT_ArguName = IFTTT_server.argName(i);
+  for (int i = 0; i < server.args(); i++) {
+    String IFTTT_ArguName = server.argName(i);
     IFTTT_ArguName.toLowerCase();
-    String IFTTT_ArgValue = IFTTT_server.arg(i);
+    String IFTTT_ArgValue = server.arg(i);
     IFTTT_ArgValue.toLowerCase();
     if (IFTTT_ArguName == "mac") {
-      DIYHUE_Register(IFTTT_ArgValue);
-      IFTTT_server.send(200, "text/plain", "Succesfull send register mac='" + IFTTT_ArgValue);
+      DIYHUE_Register(IFTTT_ArgValue);    //Register this device at the DIYHUE hub (NOT INCLUDED!!, INCLUDE YOUR OWN OR INCLUDE IT YOURSELF)
+      server.send(200, "text/plain", "Succesfull send register mac='" + IFTTT_ArgValue);
     } else {
-      IFTTT_server.send(422, "text/plain", "'mac' missing mac='" + IFTTT_ArgValue + "'");
+      server.send(422, "text/plain", "'mac' missing mac='" + IFTTT_ArgValue + "'");
     }
   }
 }
@@ -50,15 +25,15 @@ void IFTTT_handle_Set() {
   //URL argument names like "<IP>/set?mac=<value>&id=<value2>"
   //Example                 "192.168.1.128/set?mac=00:0a:95:9d:68:16&id=34"
 
-  String IFTTT_ReturnMSG;
-  int IFTTT_ReturnStatus;
+  String IFTTT_ReturnMSG = "";
+  int IFTTT_ReturnStatus = 0;
 
   String IFTTT_mac;
   int IFTTT_ID = 0;
-  for (int i = 0; i < IFTTT_server.args(); i++) {
-    String IFTTT_ArguName = IFTTT_server.argName(i);
+  for (int i = 0; i < server.args(); i++) {
+    String IFTTT_ArguName = server.argName(i);
     IFTTT_ArguName.toLowerCase();
-    String IFTTT_ArgValue = IFTTT_server.arg(i);
+    String IFTTT_ArgValue = server.arg(i);
     IFTTT_ArgValue.toLowerCase();
     if (IFTTT_ArguName == "mac") {
       IFTTT_mac = IFTTT_ArgValue;
@@ -67,7 +42,7 @@ void IFTTT_handle_Set() {
     }
   }
   if (IFTTT_mac != "" and IFTTT_ID != 0) {
-    if (!DIYHUE_sendHttpRequest(IFTTT_mac, IFTTT_ID))
+    if (!DIYHUE_sendHttpRequest(IFTTT_mac, IFTTT_ID))     //Send buttonstate to the DIYHUE hub (NOT INCLUDED!!, INCLUDE YOUR OWN OR INCLUDE IT YOURSELF)
       IFTTT_ReturnMSG = "Could not connect to DIYHUE ip='" + String(DIYHUE_bridgeIp) + "' and send Http Request";
   } else
     IFTTT_ReturnMSG = "'mac' or 'id' missing";
@@ -77,24 +52,17 @@ void IFTTT_handle_Set() {
   } else if (IFTTT_ReturnStatus == 0)
     IFTTT_ReturnStatus = 422;  //Return this error as default
 
-  IFTTT_server.send(IFTTT_ReturnStatus, "text/plain", String(IFTTT_ReturnMSG) + " mac='" + IFTTT_mac + "' id='" + String(IFTTT_ID) + "'");
+  server.send(IFTTT_ReturnStatus, "text/plain", String(IFTTT_ReturnMSG) + " mac='" + IFTTT_mac + "' id='" + String(IFTTT_ID) + "'");
   return;
 }
-void IFTTT_HandleNotFound() {
-#ifdef IFTTT_SerialEnabled
-  Serial.println("Method: " + String(IFTTT_server.method()) + " URI: " + IFTTT_server.uri());
-  for (int i = 0; i < IFTTT_server.args(); i++)
-    Serial.println("arg '" + String(IFTTT_server.argName(i)) + "' = '" + String(IFTTT_server.arg(i)) + "'");
-#endif //IFTTT_SerialEnabled
-}
+//#include <IFTTTMaker.h>
+//#define IFTTT_KEYIFTTT "REPLACE THIS WITH YOU IFTTT KEY"
+//IFTTTMaker ifttt(IFTTT_KEYIFTTT, client);
 //void IFTTT_SendIFTTT() {
-//  if (ifttt.triggerEvent("Test", "waarde 1", "waarde 2", "waarde 3")) {
-//#ifdef IFTTT_SerialEnabled
+//  if (ifttt.triggerEvent("Test", "value 1", "value 2", "value 3")) {
 //    Serial.println("IFTTT Successfully sent");
-//#endif //IFTTT_SerialEnabled
 //  } else {
-//#ifdef IFTTT_SerialEnabled
 //    Serial.println("IFTTTFailed");
-//#endif //IFTTT_SerialEnabled
 //  }
 //}
+#endif //IFTTT
